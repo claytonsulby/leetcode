16454227ab253a6a8879d02e1e661a1a1bd0483f
Clayton Sulby <48489392+claytonsulby@users.noreply.github.com>
2024-04-06 09:26:22 -0400

    Update palindrome.java
    O(n)

--- palindrome.java
+++ palindrome.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
         String stringValue = String.format("%d", x);
-        int middlePoint = stringValue.length() / 2;
-        for (int i = middlePoint; i > 0; i--) {
-            int decreasingIndice = i - 1;
-            int increasingIndice = stringValue.length() - i;
-            if (stringValue.substring(decreasingIndice) != stringValue.substring(increasingIndice)) {
+        for (int i = 0; i < stringValue.length(); i++) {
+            int decreasingIndice = stringValue.length() - i - 1;
+            int increasingIndice = i;
+            if (decreasingIndice <= increasingIndice) {
+                break;
+            }
+            char decreasingChar = stringValue.charAt(decreasingIndice);
+            char increasingChar = stringValue.charAt(increasingIndice);
+            if (decreasingSubstring != increasingSubstring) {
                 return false;
             }
         }
