1af8c1d2875a5af70a8844cb151cf8452674171b
Clayton Sulby <48489392+claytonsulby@users.noreply.github.com>
2024-04-11 17:43:50 -0400

    Update palindrome.java
    prevent overflow

--- palindrome.java
+++ palindrome.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        System.out.println(getReversedNumber(x));
+        if (x < 0) {
+            return false;
+        }
         return x == getReversedNumber(x);
     }
 
@@ -7,8 +11,13 @@ class Solution {
         int localNumber = number;
         int reversedNumber = 0;
         while (localNumber > 0) {
+            int nextShiftedBit = localNumber % 10;
+            if(reversedNumber > (Integer.MAX_VALUE - nextShiftedBit) / 10) { 
+                //check if the next iteration would be larger than the max int
+                return 0;
+            }
             reversedNumber *= 10;
-            reversedNumber += localNumber % 10;
+            reversedNumber += nextShiftedBit;
             localNumber /= 10;
         }
         return reversedNumber;
